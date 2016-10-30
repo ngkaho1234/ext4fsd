f747c70cae85b361dbc554b03f086b052d184175
Kaho Ng <ngkaho1234@gmail.com>
2016-10-30 23:13:11 +0800

    Incremental updates

--- ext4fsd/include/jbd2/jbd2_fs.h
+++ ext4fsd/include/jbd2/jbd2_fs.h
@@ -128,43 +128,43 @@ typedef struct journal_superblock_s {
 
 	/* 0x000C */
 	/* Static information describing the journal */
-	__be32	s_blocksize;					/* journal device blocksize */
-	__be32	s_maxlen;						/* total blocks in journal file */
-	__be32	s_first;						/* first block of log information */
+	__be32				s_blocksize;				/* journal device blocksize */
+	__be32				s_maxlen;					/* total blocks in journal file */
+	__be32				s_first;					/* first block of log information */
 
 	/* 0x0018 */
 	/* Dynamic information describing the current state of the log */
-	__be32	s_sequence;						/* first commit ID expected in log */
-	__be32	s_start;						/* blocknr of start of log */
+	__be32				s_sequence;					/* first commit ID expected in log */
+	__be32				s_start;					/* blocknr of start of log */
 
 	/* 0x0020 */
-	__be32	s_errno;						/* Error value, as set by jbd2_journal_abort(). */
+	__be32				s_errno;					/* Error value, as set by jbd2_journal_abort(). */
 
 	/* 0x0024 */
 	/* Remaining fields are only valid in a version-2 superblock */
-	__be32	s_feature_compat;				/* compatible feature set */
-	__be32	s_feature_incompat;				/* incompatible feature set */
-	__be32	s_feature_ro_compat;			/* readonly-compatible feature set */
+	__be32				s_feature_compat;			/* compatible feature set */
+	__be32				s_feature_incompat;			/* incompatible feature set */
+	__be32				s_feature_ro_compat;		/* readonly-compatible feature set */
 	/* 0x0030 */
-	__u8		s_uuid[UUID_SIZE];			/* 128-bit uuid for journal */
+	__u8				s_uuid[UUID_SIZE];			/* 128-bit uuid for journal */
 
 	/* 0x0040 */
-	__be32	s_nr_users;						/* Nr of filesystems sharing log */
+	__be32				s_nr_users;					/* Nr of filesystems sharing log */
 
-	__be32	s_dynsuper;						/* Blocknr of dynamic superblock copy*/
+	__be32				s_dynsuper;					/* Blocknr of dynamic superblock copy*/
 
 	/* 0x0048 */
-	__be32	s_max_transaction;				/* Limit of journal blocks per trans.*/
-	__be32	s_max_trans_data;				/* Limit of data blocks per trans. */
+	__be32				s_max_transaction;			/* Limit of journal blocks per trans.*/
+	__be32				s_max_trans_data;			/* Limit of data blocks per trans. */
 
 	/* 0x0050 */
-	__u8		s_checksum_type;			/* checksum type */
-	__u8		s_padding2[3];
-	__u32	s_padding[42];
-	__be32	s_checksum;						/* crc32c(superblock) */
+	__u8				s_checksum_type;			/* checksum type */
+	__u8				s_padding2[3];
+	__u32				s_padding[42];
+	__be32				s_checksum;					/* crc32c(superblock) */
 
 	/* 0x0100 */
-	__u8		s_users[UUID_SIZE * 48];	/* ids of all fs'es sharing the log */
+	__u8				s_users[UUID_SIZE * 48];	/* ids of all fs'es sharing the log */
 	/* 0x0400 */
 } journal_superblock_t;
 
